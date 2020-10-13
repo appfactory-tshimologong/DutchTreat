3c2ecd0835364e79a620b043283fb975a2dc38cd
Barbara Engle <barbara.engle@outlook.com>
2020-10-13 15:33:38 +0200

    added a single order using OnModelCreating in dbContext

---
+++ DutchTreat/Data/Migrations/20201013132329_addOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DutchTreat.Migrations
+{
+    public partial class addOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "OrderDate", "OrderNumber" },
+                values: new object[] { 1, new DateTime(2020, 10, 13, 13, 23, 28, 587, DateTimeKind.Utc).AddTicks(644), "12345" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
